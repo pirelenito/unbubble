817e1a26a5dcb2abaccb6b5d9a9e54cc0e543053
Christoffer <c.skeppstedt@gmail.com>
2017-12-14 13:00:09 +0100

    Avoid scrollbars in Chrome
    Add `overflow: hidden;` to avoid scrollbars popping up in Chrome, and also reset `padding` for good measure.

--- client/src/index.html.js
+++ client/src/index.html.js
@@ -24,7 +24,9 @@ export default function({ htmlWebpackPlugin }) {
           body, #root {
             width: 100%;
             height: 100%;
+            overflow: hidden;
             margin: 0;
+            padding: 0;
             background: #E64369;
           }
         </style>
